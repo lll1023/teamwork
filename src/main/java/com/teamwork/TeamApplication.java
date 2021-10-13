ad5e51cf9d75d6e399de29be93648fa7f6167989
github <2335287299@qq.com>
2021-10-13 21:34:51 +0800

    搭建框架

---
+++ src/main/java/com/teamwork/TeamApplication.java
@@ -0,0 +1,17 @@
+package com.teamwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Lsutin
+ * @Date: 2021/10/13 21:31
+ * @describe:
+ */
+@SpringBootApplication
+public class TeamApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TeamApplication.class, args);
+    }
+}
+
